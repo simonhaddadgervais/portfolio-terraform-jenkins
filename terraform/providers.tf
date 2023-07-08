bd58c4279cd46021f17dfd8efe0f0f14502150f4
simon <haddad.simon@aol.fr>
2023-07-08 17:30:23 +0200

    fix docker block for test

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,14 +4,14 @@ provider "aws" {
 }
 
 ####### DOCKER VERSION #######
-#terraform {
-#  required_providers {
-#    docker = {
-#      source  = "kreuzwerker/docker"
-#      version = "2.15.0"
-#    }
-#  }
-#}
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.15.0"
+    }
+  }
+}
 
 ###### DOCKER WITH ECR #######
 data "aws_ecr_authorization_token" "token" {}
@@ -19,11 +19,11 @@ data "aws_ecr_authorization_token" "token" {}
 locals {
   aws_ecr_url = "${var.accountId}.dkr.ecr.${var.my_region}.amazonaws.com"
 }
-#provider "docker" {
-#  registry_auth {
-#    address  = local.aws_ecr_url
-#    username = data.aws_ecr_authorization_token.token.user_name
-#    password = data.aws_ecr_authorization_token.token.password
-#  }
-#}
+provider "docker" {
+  registry_auth {
+    address  = local.aws_ecr_url
+    username = data.aws_ecr_authorization_token.token.user_name
+    password = data.aws_ecr_authorization_token.token.password
+  }
+}
 
